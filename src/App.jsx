31bed0c2050bcdd2ab37e63ea0eb02146013ae5c
Danielooo <19654989+Danielooo@users.noreply.github.com>
2025-05-30 00:40:36 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+    // maak state met te raden woord
+    // maak input voor letters
+    // maak submit button
+
+  return (
+    <>
+        <section className='outer-box'>
+            <div className='inner-box'>
+                <h1>GALGJE</h1>
+
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default App
